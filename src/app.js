67c9fdbc9894d33eeac8bd1c85c27e48425e84fb
San Baby Francis <san.baby.francis123@gmail.com>
2023-02-21 19:10:24 +0530

    configure music player, timer

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ import CreateNewForm from "./components/CreateNewForm";
 import TaskCard from "./components/TaskCard";
 import ReactJkMusicPlayer from "react-jinke-music-player";
 import 'react-jinke-music-player/assets/index.css';
+import TimerWrapper from "./components/Timer";
+
 
 function App() {
     const [tasks, setTasks] = useState([])
@@ -22,18 +24,33 @@ function App() {
         go()
     }, [])
 
+    const audioList = [
+    {
+        musicSrc: "./music/letter-to-a-friend.mp3",
+        cover: "./music/letter-to-a-friend.jpeg",
+        name: "Letter to a Friend",
+        singer: "Robert Gromotka"
+    },
+    {
+        musicSrc: "./music/leaves-from-the-vine.mp3",
+        cover: "./music/leaves-from-the-vine.jpeg",
+        name: "Leaves from the Vine",
+        singer: "Samuel Kim"
+    }]
+
     return (
         <div>
             <HeaderBar email={users.email} />
             <div className="container">
                 <div className="task-grid">
                     <CreateNewForm setTasks={setTasks} />
+                    {/* <TimerWrapper /> */}
                     {tasks.map(function (task) {
                         return <TaskCard key={task._id} name={task.name} description={task.description} deadline={task.deadline} id={task._id} setTasks={setTasks} />
                     })}
                 </div>
             </div>
-            <ReactJkMusicPlayer toggleMode={false} mode="full" />
+            <ReactJkMusicPlayer audioLists={audioList} theme="dark" mode="full" autoPlay={false} toggleMode={false} responsive={false} showThemeSwitch={false} showDownload={false} showPlayMode={false} />
         </div>
     );
 }
--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function HeaderBar(props) {
     return (
-        <nav className="navbar navbar-expand-lg py-3 navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg py-3 navbar-dark bg-dark header-font-prop">
             <div className="container">
                 <h1>
                     <a style={{textDecoration: 'none'}} className="text-white" href="/">EMPROVE</a>
