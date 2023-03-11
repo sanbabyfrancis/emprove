da35574f8217c6b30290215fe3ea8a7b1ae68375
San Baby Francis <san.baby.francis123@gmail.com>
2023-03-11 10:21:08 +0530

    added header links

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -5,9 +5,13 @@ function HeaderBar(props) {
         <nav className="navbar navbar-expand-lg py-3 navbar-dark bg-dark header-font-prop">
             <div className="container">
                 <h1>
-                    <a style={{textDecoration: 'none'}} className="text-white" href="/">EMPROVE</a>
+                    <a style={{ textDecoration: 'none' }} className="text-white" href="/">EMPROVE</a>
                 </h1>
-                <p className="text-white" style={{textAlign: "right"}}>{props.email}</p>
+                <ul className="nav-links" style={{ display: "flex", listStyleType: "none" }}>
+                    <li><p className="text-white">{props.email} &nbsp;&nbsp;</p></li>
+                    <li><a style={{ textDecoration: 'none' }} className="text-white" href="#">| Productivity report &nbsp;&nbsp;</a></li>
+                    <li><a style={{ textDecoration: 'none' }} className="text-white" href="/">| Sign out</a></li>
+                </ul>
             </div>
         </nav>
     )
